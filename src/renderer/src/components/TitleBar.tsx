0b549734b8ce465d5969340dc342b2bcaed07d44
mival <mivalek@gmail.com>
2024-07-19 00:18:15 +0200

    chore: added app icons

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,10 +1,11 @@
 import { Component } from 'solid-js'
+// import icon from '../../../../resources/icon.png?asset'
 
 export const TitleBar: Component = () => {
   return (
     <div id="titleBarContainer" class="absolute top-0 w-full">
       <div id="titleBar" class="draggable h-[30.5px] bg-background/40">
-        {/* <span class="draggable">Example PWA</span> */}
+        {/* <img src={icon} class="max-h-full px-2 py-1.5" /> */}
         {/* <input class="nonDraggable" type="text" placeholder="Search"></input> */}
       </div>
     </div>
