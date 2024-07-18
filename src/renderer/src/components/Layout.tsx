9cd86d0120526a5b4ee6f79810cc9fd40b24a84f
mival <mivalek@gmail.com>
2024-07-19 00:17:54 +0200

    style: fixed layout

--- src/renderer/src/components/Layout.tsx
+++ src/renderer/src/components/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout: Component<{ children: JSXElement }> = (props) => {
         id="app-container"
         class="grid h-[calc(100vh_-_30.5px)] mt-[30.5px] relative overflow-clip"
         style={{
-          'grid-template-columns': 'minmax(auto, 200px) auto auto'
+          'grid-template-columns': 'minmax(auto, 200px) minmax(auto, 1fr) auto'
         }}
       >
         {props.children}
